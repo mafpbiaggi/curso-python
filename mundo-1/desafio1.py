19a13b072805454b08b2c2407577b40f57630a36
Marco Aurélio Biaggi <mafpbiaggi@gmail.com>
2025-10-16 10:20:57 -0300

    curso-python(add): Criação do script 'desafio1.py'.

---
+++ mundo-1/desafio1.py
@@ -0,0 +1,8 @@
+# Desafio 1:
+# Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+# Exemplo da saída: "Olá %nome%! Prazer em te conhecer!"
+
+print("====== Desafio 1 ======")
+nome = input("Qual é o seu nome?\n")
+
+print("Ola", nome + "! Prazer em te conhecer!")
