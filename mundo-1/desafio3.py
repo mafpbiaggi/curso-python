fe13e501e0b4403af1fc555b3979768175097dfc
Marco Aurélio Biaggi <mafpbiaggi@gmail.com>
2025-10-16 13:06:56 -0300

    curso-python(add): Criação do script 'mundo-1/desafio3.py'.

---
+++ mundo-1/desafio3.py
@@ -0,0 +1,13 @@
+# Desafio 3:
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+# Saída esperada: A soma é %resultado%.
+
+# Exibe o cabeçalho
+print("====== Desafio 3 ======")
+
+# Recebe os valores digitado pelo usuário, já em formato numérico utilizando a função int()
+num1 = int(input("Primeiro número:"))
+num2 = int(input("Segundo número:"))
+
+# Exibe o resultado com a conta sendo feita na mesma linha.
+print("A soma é", (num1+num2))
