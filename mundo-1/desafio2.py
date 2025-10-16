f4744aab428b608450431f75b9d93bce41d57c1a
Marco Aurélio Biaggi <mafpbiaggi@gmail.com>
2025-10-16 12:35:51 -0300

    curso-python(add): Criação do script 'mundo-1/desafio2.py'.

---
+++ mundo-1/desafio2.py
@@ -0,0 +1,14 @@
+# Desafio 2:
+# Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+# Exemplo da saída: "Você nasceu no dia 17 de Mar de 1978."
+
+# Exibe o cabeçalho
+print("====== Desafio 2 ======")
+
+# Coleta os dados do usuário e os armazena em variáveis
+dia = input("Dia = ")
+mes = input("Mês = ")
+ano = input("Ano = ")
+
+# Exibe a resposta na tela
+print("Você nasceu no dia", dia, "de", mes, "de", ano + ".")
