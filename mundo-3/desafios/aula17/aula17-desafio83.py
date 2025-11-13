5cd4ac96f56ba8f8e4945a22a6f0c12691da1f40
Marco Aurélio Biaggi <mafpbiaggi@gmail.com>
2025-11-13 17:51:29 -0300

    curso-python(upd/mundo3): Resolução e correção do desafio 83.
    Esse exercício serve como resultado para um desafio do Leet Code.

--- mundo-3/desafios/aula17/aula17-desafio83.py
+++ mundo-3/desafios/aula17/aula17-desafio83.py
@@ -1,9 +1,63 @@
 '''
 Desafio 83: https://youtu.be/N1hTsbW50eM?t=2223
-Correção:
+Correção: https://youtu.be/dvhP41Z7TLk
 
-Enunciado:
-Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
+Enunciado:''' 
+sub = ' Validador de Expressão '
+'''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos
 e fechados na ordem correta.
+'''
+
+t_sub = int((52 - len(sub)) / 2)
+print('=' * 20 + ' Desafio 82 ' + '=' * 20)
+print('-' * t_sub + sub + '-' * t_sub)
+
+expr = str(input('\nDigite a expressão: '))
+aux = []
+
+for c in expr:
+    if c == '{' or c == '[' or c == '(':
+        aux.append(c)
+    elif c == ')':
+        if len(aux) > 0 and aux[-1] == '(':
+            aux.pop()
+        else:
+            aux.append(c)
+            break
+    elif c == ']':
+        if len(aux) > 0 and aux[-1] == '[':
+            aux.pop()
+        else:
+            aux.append(c)
+            break
+    elif c == '}':
+        if len(aux) > 0 and aux[-1] == '{':
+            aux.pop()
+        else:
+            aux.append(c)
+            break
+if len(aux) == 0:
+    print(True)
+else:
+    print(False)
+
+'''
+# Solução do professor: 
+expr = str(input('Digite a expressão: '))
+pilha = []
+
+for símb in expr:
+    if símb == '(':
+        pilha.append('(')
+    elif símb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida.')
+else:
+    print('Sua expressão está errada.')
 '''
