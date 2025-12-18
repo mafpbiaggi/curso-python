c976fb1fda7207fcdb969e3b70f9bd956c00ff72
Marco Aurélio Biaggi <mafpbiaggi@gmail.com>
2025-12-18 18:11:58 -0300

    curso-python(add/mundo3): Criação dos arquivos de desafios 101 à 106.

--- mundo-3/desafios/aula21/aula20-desafio106.py
+++ mundo-3/desafios/aula21/aula20-desafio106.py
@@ -1,11 +1,14 @@
 '''
-Desafio 105: 
+Desafio 106: https://youtu.be/etjJ_4Eqrk8?t=2825
 Correção: 
 
 '''
-sub = ''
+sub = 'Interactive helping system in Python'
 
 '''
 Enunciado:
+Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando
+e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará.
 
+Obs: Utilize cores.
 '''
